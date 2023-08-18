8c3febf35f2dc7b8ba82befb3d9a7998c934bcd6
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2023-08-18 20:55:21 -0300

    first commit: arquitetura basica do projeto

---
+++ src/main/java/com/projects/bem_estar/BemEstarApplication.java
@@ -0,0 +1,13 @@
+package com.projects.bem_estar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemEstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemEstarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/bem_estar/BemEstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.bem_estar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemEstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
